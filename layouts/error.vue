2a139280893eee0f1aace6a490e598d83d835758
pedr0vict0r <pedrovictorcosta7@gmail.com>
2021-04-06 15:29:56 -0300

    feat(error.vue): adicionando layout de pagina de erro

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">Não há nada aqui</h1>
+    <h1 v-else>Ocorreu um erro - {{ error.statusCode }}</h1>
+    <NuxtLink to="/">Início</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'error',
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['error'], // you can set a custom layout for the error page
+}
+</script>
