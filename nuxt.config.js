680e1dd6e69fcdcc7a4ae838c823968940a95266
pedr0vict0r <pedrovictorcosta7@gmail.com>
2021-05-02 17:45:06 -0300

    feat(nuxt.config.js): metatags

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,52 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: 'Portifolio de Pedro Nascimento',
+        content: 'Um Desenvolvedor de Belém do Pará',
+      },
+      {
+        hid: 'og:type',
+        property: 'og:type',
+        content: 'website',
+      },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'Pedro Nascimento Developer',
+      },
+      {
+        hid: 'og:locale',
+        property: 'og:locale',
+        content: 'pt_BR',
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://pedronascimento.dev.br/',
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Pedro Nascimento Developer',
+      },
+      {
+        hid: 'og:description',
+        name: 'og:description',
+        content: 'Um Desenvolvedor de Belém do Pará',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://avatars.githubusercontent.com/u/17133455',
+      },
+      {
+        hid: 'og:image:width',
+        property: 'og:image:width',
+        content: '1024',
+      },
+      {
+        hid: 'og:image:height',
+        property: 'og:image:height',
+        content: '1024',
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
