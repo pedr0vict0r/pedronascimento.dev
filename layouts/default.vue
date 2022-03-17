a9adfa9c20543f07aed64f974ac68f2a1606b943
Pedro Nascimento <pedrovictorcosta7@gmail.com>
2022-03-17 13:46:33 -0300

    fix: adjust lint

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,9 +9,7 @@ html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
+  word-spacing: 1px;/* -ms-text-size-adjust: 100%; *//* -webkit-text-size-adjust: 100%; */
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
@@ -24,7 +22,7 @@ html {
   margin: 0;
 }
 
-.button--green {
+.button-green {
   display: inline-block;
   border-radius: 4px;
   border: 1px solid #3b8070;
@@ -33,12 +31,12 @@ html {
   padding: 10px 30px;
 }
 
-.button--green:hover {
+.button-green:hover {
   color: #fff;
   background-color: #3b8070;
 }
 
-.button--grey {
+.button-grey {
   display: inline-block;
   border-radius: 4px;
   border: 1px solid #35495e;
@@ -48,7 +46,7 @@ html {
   margin-left: 15px;
 }
 
-.button--grey:hover {
+.button-grey:hover {
   color: #fff;
   background-color: #35495e;
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -97,7 +97,10 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
